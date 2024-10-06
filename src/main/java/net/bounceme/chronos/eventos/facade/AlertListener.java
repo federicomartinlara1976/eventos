b8be2a5dca5d25c9ebcf927f362bbf218ff108e9
federicomartinlara1976@gmail.com <federicomartinlara1976@gmail.com>
2024-10-06 11:18:34 +0200

    Se añaden notificaciones

--- src/main/java/net/bounceme/chronos/eventos/facade/AlertListener.java
+++ src/main/java/net/bounceme/chronos/eventos/facade/AlertListener.java
@@ -1,11 +1,13 @@
 package net.bounceme.chronos.eventos.facade;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 import net.bounceme.chronos.eventos.dto.TransferDTO;
+import net.bounceme.chronos.notifications.services.NotificationService;
 
 @Component
 @Slf4j
@@ -13,12 +15,17 @@ public class AlertListener {
 
 	@Value("${amountAlertLimit}")
 	private Integer amountAlertLimit;
+	
+	@Autowired
+	private NotificationService notificationService;
 
 	@EventListener
 	public void sendAlertIfNeeded(TransferDTO transfer) {
 		if (transfer.getAmount() > amountAlertLimit) {
 			log.info("La cantidad {} supera el límite establecido a {}. Se enviará alerta", transfer.getAmount(), amountAlertLimit);
-		} else {
+			notificationService.sendNotification("eventos", "La cantidad supera el límite establecido", "WARNING");
+		} 
+		else {
 			log.info("Cantidad {} dentro del límite", transfer.getAmount());
 		}
 	}
